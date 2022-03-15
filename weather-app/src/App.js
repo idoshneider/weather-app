f1074bc0e8a4c166ad16837f3fa4e2a711e09daf
idoshneider <ido.shn@gmail.com>
2022-03-15 11:57:47 +0200

    added timezone

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,7 @@
 import {useState} from 'react' ; 
 import './App.css';
 import axios from 'axios';
-//updated
+//check
 import Clock from 'react-live-clock';
 function App() {
   const [data,setData] = useState({});
